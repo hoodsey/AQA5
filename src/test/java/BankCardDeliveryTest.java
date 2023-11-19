875012f191ae0e5add82cd53b2854f7de5c1a6f1
Nasya Kabakova <kabakova.nas@gmail.com>
2023-11-19 19:35:37 +0300

    edit tests

--- src/test/java/BankCardDeliveryTest.java
+++ src/test/java/BankCardDeliveryTest.java
@@ -2,27 +2,22 @@ import com.github.javafaker.Faker;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
-import ru.netology.DataGenerator;
+import ru.netology.model.DataGenerator;
 import ru.netology.model.BankCardItem;
 
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
+import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class BankCardDeliveryTest {
-    public String generateDate(int days) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
 
 
     @BeforeEach
     public void setUp() {
-        Faker faker = new Faker();
         open("http://localhost:9999");
         $("[data-test-id='date'] [placeholder='Дата встречи']").sendKeys(Keys.CONTROL, "a" + Keys.DELETE);
 
@@ -44,14 +39,12 @@ public class BankCardDeliveryTest {
         $("[data-test-id='agreement']").click();
         $(".button").click();
         $(".notification__closer").click();
-        Thread.sleep(5000);
-        $("[data-test-id='city'] input").clear();
+        $("[data-test-id='date'] [placeholder='Дата встречи']").sendKeys(Keys.CONTROL, "a" + Keys.DELETE);
         $("[data-test-id='date'] input").val(secondMeetingDate);
         $(".button").click();
-        $("[data-test-id='replan-notification']").shouldHave(exactText("Необходимо подтверждение\n" +
-                "У вас уже запланирована встреча на другую дату. Перепланировать?\n" +
-                "\n" +
-                "Перепланировать"));
+        $("[data-test-id='replan-notification'] button").click();
+        $("[data-test-id='success-notification']").shouldHave(exactText("Успешно!\n" +
+                "Встреча успешно запланирована на " + secondMeetingDate));
     }
 
 
@@ -72,44 +65,55 @@ public class BankCardDeliveryTest {
     }
 
     @Test
-    void CityWithOutCatalog() {
-        var item = new BankCardItem("Кабакова Анастасия", "Канадлакша", generateDate(6).toString(), "+79818042544");
-        $("[data-test-id='city'] input").setValue(item.getCity());
-        $("[data-test-id='date'] input").val(item.getDate());
-        $("[data-test-id='name'] input").setValue(item.getName());
-        $("[data-test-id='phone'] input").setValue(item.getPhone());
+    void cityWithOutCatalog() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        Faker faker = new Faker(new Locale("en"));
+        var city = faker.address().city();
+        $("[data-test-id='city'] input").setValue(city);
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
         $("[data-test-id='agreement']").click();
         $(".button").click();
         $("[data-test-id='city'] [class='input__sub']").shouldBe(ownText("Доставка в выбранный город недоступна"));
     }
 
     @Test
-    @DisplayName("Имя и Фамилия указаные неверно.")
-    void IncorrectName() {
-        var item = new BankCardItem("Anastasiia 123", "Краснодар", generateDate(6).toString(), "+79818042544");
-        $("[data-test-id='city'] input").setValue(item.getCity());
-        $("[data-test-id='date'] input").val(item.getDate());
-        $("[data-test-id='name'] input").setValue(item.getName());
-        $("[data-test-id='phone'] input").setValue(item.getPhone());
+    void incorrectName() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        Faker faker = new Faker(new Locale("en"));
+        var name = faker.internet().emailAddress();
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").setValue(name);
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
         $("[data-test-id='agreement']").click();
         $(".button").click();
         $("[data-test-id='name'] [class='input__sub']").shouldHave(ownText("Имя и Фамилия указаные неверно."));
     }
 
     @Test
-    void IncorrectPhone() {
-        var item = new BankCardItem("Кабакова Анастасия", "Краснодар", generateDate(6).toString(), "98818042544");
-        $("[data-test-id='city'] input").setValue(item.getCity());
-        $("[data-test-id='date'] input").val(item.getDate());
-        $("[data-test-id='name'] input").setValue(item.getName());
-        $("[data-test-id='phone'] input").setValue(item.getPhone());
+    void incorrectPhone() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        Faker faker = new Faker(new Locale("en"));
+        var name = faker.phoneNumber().subscriberNumber(5);
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
         $("[data-test-id='agreement']").click();
         $(".button").click();
         $("[data-test-id='phone'] [class='input__sub']").shouldHave(ownText("Телефон указан неверно."));
     }
 
     @Test
-    void WithOutAgree() {
+    void withOutAgree() {
         var validUser = DataGenerator.Registration.generateUser("ru");
         var daysToAddForFirstMeeting = 4;
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
